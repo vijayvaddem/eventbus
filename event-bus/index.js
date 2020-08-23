6a2b794e860eb5990f716d520f7beadd402655eb
vaddv <vijay.vaddem@gmail.com>
2020-08-23 21:39:41 +0530

    added docker files

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,8 +10,12 @@ app.listen("4005", () => {
   console.log("listening on 4005");
 });
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/event", event);
   axios.post("http://localhost:4001/event", event);
@@ -19,4 +23,8 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4003/event", event);
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
