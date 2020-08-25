e38427515a6e0bbd5aa39a5a300c56a95b7d4cb3
vaddv <vijay.vaddem@gmail.com>
2020-08-26 01:18:56 +0530

    deployed event-bus  as a service in a pod and enabled clusterIP based access

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,10 +17,10 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/event", event);
-  axios.post("http://localhost:4001/event", event);
-  axios.post("http://localhost:4002/event", event);
-  axios.post("http://localhost:4003/event", event);
+  axios.post("http://posts-clusterip-srv:4000/event", event);
+  // axios.post("http://localhost:4001/event", event);
+  // axios.post("http://localhost:4002/event", event);
+  // axios.post("http://localhost:4003/event", event);
 
   res.send({ status: "OK" });
 });
